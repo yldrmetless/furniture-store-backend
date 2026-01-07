aab4a1fcb9daf571c403af958c57b002a44429d1
Metehan Yıldırım <metehan480@gmail.com>
2026-01-07 03:35:17 +0300

    fix

--- furniturestore/config/urls.py
+++ furniturestore/config/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/products/", include("products.urls")),
     path("api/blog/", include("blog.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r"^media/(?P<path>.*)$", static_serve, {"document_root": settings.MEDIA_ROOT}),
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
