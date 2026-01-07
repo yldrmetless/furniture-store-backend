d66622d2e4e2837e9ce76d5b34827ff87afab539
Metehan Yıldırım <metehan480@gmail.com>
2026-01-07 03:12:14 +0300

    v2

--- furniturestore/config/settings.py
+++ furniturestore/config/settings.py
@@ -165,7 +165,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "/var/data/media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
